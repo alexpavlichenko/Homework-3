d8503030bc46159674a1a18074970b437e6b4098
Александр <alexpavlich@yandex.ru>
2022-03-31 00:19:26 +0300

    Задание 2

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -6,15 +6,24 @@ public class Main {
     // Задание 1
 
     public static void main(String[] args) {
-        int clientOs = 0;
+        int clientOs = 1;
         if (clientOs == 0) {
-        System.out.println("Download IOS App");
-    }
+            System.out.println("Download IOS App");
+        }
         if (clientOs == 1) {
             System.out.println("Download Android App");
         }
+        // Задание 2
+        {
+            int clientDeviceYear = 2016;
+            if (clientDeviceYear < 2015 && clientOs == 0) {
+                System.out.println("Download IOS lite version");
+            } else if (clientDeviceYear < 2015 && clientOs == 1 ) {
+                System.out.println("Download Android lite version");
+            }
         }
+    }
+}
+
 
     // Задание 2
-
-}
