ec7d1ad715b1f173eefb4112281dc24a221bc239
Александр <alexpavlich@yandex.ru>
2022-03-30 22:24:07 +0300

    Задание 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,20 @@
 package ru.skypro;
 
+import java.sql.SQLOutput;
+
 public class Main {
+    // Задание 1
 
     public static void main(String[] args) {
-	// write your code here
+        int clientOs = 0;
+        if (clientOs == 0) {
+        System.out.println("Download IOS App");
     }
+        if (clientOs == 1) {
+            System.out.println("Download Android App");
+        }
+        }
+
+    // Задание 2
+
 }
