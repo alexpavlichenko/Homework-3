6c7031de9bf98db2ab5c34f34c7e385447912fbc
Alexpavlichenko <alexpavlich@yandex.ru>
2022-03-31 17:20:43 +0300

    Задание 4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -6,7 +6,7 @@ public class Main {
     // Задание 1
 
     public static void main(String[] args) {
-        int clientOs = 1;
+        int clientOs = 0;
         if (clientOs == 0) {
             System.out.println("Download IOS App");
         }
@@ -15,15 +15,35 @@ public class Main {
         }
         // Задание 2
         {
-            int clientDeviceYear = 2016;
+            int clientDeviceYear = 2013;
             if (clientDeviceYear < 2015 && clientOs == 0) {
-                System.out.println("Download IOS lite version");
-            } else if (clientDeviceYear < 2015 && clientOs == 1 ) {
-                System.out.println("Download Android lite version");
+                System.out.println("Download IOS lite app version");
+            } else if (clientDeviceYear < 2015 && clientOs == 1) {
+                System.out.println("Download Android lite app version");
             }
+        }
+        //Задание 3
+        {
+            int year = 400;
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println(year + " is a leap year");
+            } else {
+                System.out.println(year + " isn't a leap year");
+            }
+        }
+        //Задание 4
+        {
+            int deliveryDistance = 95;
+            if (deliveryDistance < 20) {
+                System.out.println("Delivery will take 1 day");
+            } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Delivery will take 2 days");
+        } else {
+            System.out.println("Delivery will take 3 days");
+        }
+
         }
     }
 }
 
 
-    // Задание 2
